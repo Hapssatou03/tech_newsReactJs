fba6036f0ba3d8ec28e7aae4a6887bb04b31049d
babaHedz <hedi.rivas.pro@gmail.com>
2024-03-05 16:41:06 +0100

    first commit

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -19,5 +20,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 module.exports = app;
